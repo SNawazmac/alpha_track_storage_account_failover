78d0e348faf3068875d104861b4667a15e73bbcd
S Nawaz <95207462+SNawazmac@users.noreply.github.com>
2023-04-19 14:48:46 +0530

    Update Invoke_Failover_Storage_multi-values_param.ps1

--- Invoke_Failover_Storage_multi-values_param.ps1
+++ Invoke_Failover_Storage_multi-values_param.ps1
@@ -1,21 +1,21 @@
 param(
-[Parameter(Mandatory=$true)][string]$resource_group_name,       #Enter the resourcegroup name of the Storageaccount
-[Parameter(Mandatory=$true)][array]$storageaccount_names,       #Enter the storage account name(s) on which failover has to be initiated
+[Parameter(Mandatory=$true)][string]$storage_account_resource_group_name,       #Enter the resourcegroup name of the Storageaccount
+[Parameter(Mandatory=$true)][array]$private_storage_account_names,       #Enter the storage account name(s) on which failover has to be initiated
 [Parameter(Mandatory=$true)][string]$sku                        #Enter the SKU as Standard_GRS to re-enable geo-replication post failover
 )
 
  
-$storageaccounts = $storageaccount_names.split(",")
-foreach($storageaccount_name in $storageaccounts)
+$storage_accounts = $private_storage_account_names.split(",")
+foreach($storage_account_name in $storage_accounts)
 {
 #Below command Invokes failover on the selected storage account(s)
-$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $resource_group_name -Name $storageaccount_name -Force -AsJob
+$failover = Invoke-AzStorageAccountFailover -ResourceGroupName $storage_account_resource_group_name -Name $storage_account_name -Force -AsJob
 }
 
 Wait-Job $failover
 
-foreach($storageaccount_name in $storageaccounts)
+foreach($storage_account_name in $storage_accounts)
 {
 #Below command updates the SKU of the storage account(s) to Standard_GRS post failover
-Set-AzStorageAccount -ResourceGroupName $resource_group_name -Name $storageaccount_name -SkuName $sku -Force
+Set-AzStorageAccount -ResourceGroupName $storage_account_resource_group_name -Name $storage_account_name -SkuName $sku -Force
 }
